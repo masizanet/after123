7793fd69fa71ca787a33c64e718b1ef3d5bb2937
resistan <resistan@gmail.com>
2025-01-01 19:18:19 +0900

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Footer } from '@/components/Footer';
 import styles from './layout.module.css';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: '123 내란 관련 주요 의안',
+  title: '12.3 내란 관련 주요 의안',
   description: '의안 표결 결과 및 관련 정보 모음',
 };
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className={styles.footer}>
       <p className={styles.footerText}>
         <a href='https://open.assembly.go.kr/portal/openapi/main.do' target='_blank' >열린국회정보</a>의 데이터를 활용하여 제작되었습니다.<br />
-        기능의 오류가 발생할 수 있습니다.
+        기능상 오류가 발생할 수 있습니다.
       </p>
     </footer>
   );
