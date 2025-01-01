75ee195b05d79b2713cc5891f9502fc24243c5b8
resistan <resistan@gmail.com>
2025-01-01 19:09:36 +0900

    add footer, update layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footerText}>
+        <a href='https://open.assembly.go.kr/portal/openapi/main.do' target='_blank' >열린국회정보</a>의 데이터를 활용하여 제작되었습니다.<br />
+        기능의 오류가 발생할 수 있습니다.
+      </p>
+    </footer>
+  );
+}
