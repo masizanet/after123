62b391af8900e06445e9f4a0313f0a55c7bd226b
resistan <resistan@gmail.com>
2025-01-07 00:12:14 +0900

    update bills

--- src/components/BillList.tsx
+++ src/components/BillList.tsx
@@ -4,24 +4,24 @@ import { formatDate } from '@/lib/utils/date';
 import Link from 'next/link';
 import styles from './BillList.module.css';
 import type { Bill } from '@/types/bill';
-// import { getPartyColor } from '@/constants/partyColors';
+import { getPartyColor } from '@/constants/partyColors';
 
 interface BillListProps {
   bills: Bill[];
 }
 
-// function extractPartyFromPPSR(ppsr: string): string {
-//   const parties = [
-//     '더불어민주당', '국민의힘', '조국혁신당', 
-//     '개혁신당', '진보당', '기본소득당', '사회민주당'
-//   ];
-//   for (const party of parties) {
-//     if (ppsr.includes(party)) {
-//       return party;
-//     }
-//   }
-//   return '';
-// }
+function extractPartyFromPPSR(ppsr: string): string {
+  const parties = [
+    '더불어민주당', '국민의힘', '조국혁신당', 
+    '개혁신당', '진보당', '기본소득당', '사회민주당'
+  ];
+  for (const party of parties) {
+    if (ppsr.includes(party)) {
+      return party;
+    }
+  }
+  return '';
+}
 
 export function BillList({ bills }: BillListProps) {
   return (
@@ -30,9 +30,8 @@ export function BillList({ bills }: BillListProps) {
         <thead>
           <tr>
             <th scope="col">의안번호</th>
-            <th scope="col">의안명</th>
+            <th scope="col">의안명 - 제안자</th>
             <th scope="col">제안일</th>
-            <th scope="col">제안자</th>
             <th scope="col">처리일</th>
             <th scope="col">처리결과</th>
             <th scope="col">원문</th>
@@ -41,30 +40,31 @@ export function BillList({ bills }: BillListProps) {
         <tbody>
           {bills.map((bill) => {
             const hasVoteResult = bill.hasVoteResult && bill.BILL_NO !== '2206205';
-            // const party = extractPartyFromPPSR(bill.PPSR);
+            const party = extractPartyFromPPSR(bill.PPSR);
 
             return (
               <tr key={bill.BILL_ID} className={styles.item}>
                 <td className={styles.number}>{bill.BILL_NO}</td>
                 <td className={styles.info}>
                   {bill.BILL_NM}
+                  <br/>
+                  <span className={styles.knds}>
+                    {bill.PPSR_KND === '의원' && bill.PPSR.includes('의원') && (
+                      <span className={styles.partyName}>
+                        {'('}
+                        <span 
+                          className={styles.partyColor} 
+                          style={{ 
+                            backgroundColor: getPartyColor(party).main 
+                          }} 
+                        />
+                        {bill.PPSR}
+                      </span>
+                    )}
+                    {'- '}{bill.PPSR_KND !== '의원' && bill.PPSR}
+                  </span>
                 </td>
                 <td className={styles.date}>{formatDate(bill.PPSL_DT)}</td>
-                <td className={styles.value}>
-                  {bill.PPSR_KIND}
-                  {/* {bill.PPSR_KND === '의원' && bill.PPSR.includes('의원') && (
-                    <span className={styles.partyName}>
-                      <span 
-                        className={styles.partyColor} 
-                        style={{ 
-                          backgroundColor: getPartyColor(party).main 
-                        }} 
-                      />
-                      {bill.PPSR}
-                    </span>
-                  )}
-                  {bill.PPSR_KND !== '의원' && bill.PPSR} */}
-                </td>
                 <td className={styles.date}>{bill.RGS_RSLN_DT ? formatDate(bill.RGS_RSLN_DT) : '-'}</td>
                 <td className={styles.result}>
                   <span>{bill.RGS_CONF_RSLT || '-'}</span>
